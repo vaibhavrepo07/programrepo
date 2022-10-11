20b956bb70edd5d2b4335d0e62a3705c0fc1958d
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-10-12 00:11:51 +0530

    if statement for age

---
+++ if statemet for age.cpp
@@ -0,0 +1,19 @@
+//Program for age with if statement 
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int age ;
+	cout<<"Enter the age of the person "<<endl;
+	cin>> age;
+	if(age>20 && age<30)
+	cout<<"your age is "<<age<<endl;
+	cout<<"you can go for update your adhar card"<<endl;
+	
+	
+	if(age>15)
+	cout<<"your can give your pan card"<<endl;
+	cout<<"Its time to give your cellphone number"<<endl;
+	
+	return 0 ;
+}
