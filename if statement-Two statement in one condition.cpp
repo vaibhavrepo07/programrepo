702975ba15ce1faab97079bc5f2f983551fb80c7
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-10-12 00:08:52 +0530

    Two condition in one condition

---
+++ if statement-Two statement in one condition.cpp
@@ -0,0 +1,14 @@
+//Progarm for two statements in one condition
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int a ;
+	cout<<"What if we Coding with if Statement "<<endl;
+	cin>>a;
+	if(a)
+	cout<<"Hello"<<endl;
+	cout<<"Brother"<<endl;
+	
+	return 0;
+}
