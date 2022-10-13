f3f73cdf0ca9466770e661438dede53bd0e4c8a9
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-10-13 06:10:55 +0530

    For loop to write increment in different position

---
+++ Program for For loop to wirte incremnt in different poistion.cpp
@@ -0,0 +1,13 @@
+//Write a for Loop progaram 
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int i , j;
+	for(i=1,j=0 ;i<5,j<=6;j++)
+	{
+		cout<<i<<j<<endl;
+		i++;
+	}
+	return 0;
+}
