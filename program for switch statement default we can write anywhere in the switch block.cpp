10408a56c23bc37e242714cdb664f8862491ed97
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-10-13 06:12:43 +0530

    Switch block

---
+++ program for switch statement default we can write anywhere in the switch block.cpp
@@ -0,0 +1,20 @@
+//progarm for switch statements default can write anywhere within switch block 
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int a ;
+	cout<<"Switch block is executed"<<endl;
+	cin>> a;
+	switch(a)
+	{
+		default: 
+		cout<<"Enter number"<<endl;
+		case 1 :cout<<"Look Forword"<<endl;
+		break;
+		case 2: cout<<"Look Ahead"<<endl;
+		break;
+		
+		
+	}
+}
