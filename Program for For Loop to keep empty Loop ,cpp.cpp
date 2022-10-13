f8cb4b8a03b4b99d1398750b2799679b838677a6
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-10-13 06:07:45 +0530

    For Loop to keep empty loop

---
+++ Program for For Loop to keep empty Loop ,cpp.cpp
@@ -0,0 +1,14 @@
+//Program for For loop to keep empty loop
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int i=1;
+	cout<<"Coding with For loop to keep empty Loop "<<endl;
+	for( ; i<=10 ; )
+	{
+		cout<<i<<endl;
+		i++;
+	}
+	return 0 ;
+}
