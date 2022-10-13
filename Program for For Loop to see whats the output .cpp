24a740f0fb504b2b5961ee18208e71dd5e81f9ac
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-10-13 06:09:28 +0530

    For Loop to see what the output

---
+++ Program for For Loop to see whats the output .cpp
@@ -0,0 +1,15 @@
+//Program for For Loop 
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	cout<<"Let's see the out put of For Loop"<<endl;
+	for(int i=10;i<10; )
+	{
+		cout<<i<<endl;
+		i++;
+	}
+	return 0;
+	
+}
+
