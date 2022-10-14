37603f3599b5b5e4115ff19c2e1ece93f8f0fc9f
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-10-14 11:22:24 +0530

    Switch statment1

---
+++ SwitchStatement.cpp
@@ -0,0 +1,22 @@
+//Program for Switch Statements
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int a ;
+	cout<<"Let's do Coding wiht Switch statements"<<endl;
+	cin>> a;
+	switch(a)
+	{
+		case 1: cout<<"Hello "<<endl;
+		break;
+		case 2: cout<<"How ?"<<endl;
+		break;
+		case 3: cout<<"Are You"<<endl;
+		break;
+		
+		
+	default :
+		cout<<"out"<<endl;
+	}
+}
