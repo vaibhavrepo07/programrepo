b70af8a6af8e2106764033221f25f2597c1bf4e1
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-10-14 11:31:33 +0530

    Total Number of Odd and Even Number

---
+++ Write a program for array for count total number of Even and odd element .cpp
@@ -0,0 +1,20 @@
+//WAP to read an array of 10 integer and count total number of Even and odd elements 
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int a[10],i , even, odd ;
+	cout<<"Enter the 10 Elements in an Array"<<endl;
+
+	for(i=0  ; i==i+2 ;i++)      // This for Loop's purpose is to store the data 
+	{
+		cin>>a[i];
+		cout<<"This is even Number "<<endl;
+	}
+	for(i=0 ; i<=10 ;i++)
+	{
+		cout<< a[i]<<endl;
+	}
+ return 0 ;
+ } 
