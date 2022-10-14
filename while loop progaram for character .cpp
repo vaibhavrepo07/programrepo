d739c7e95bc63bd74ae7efe4277e30784ccbab7e
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-10-14 11:26:05 +0530

    While loop

---
+++ while loop progaram for character .cpp
@@ -0,0 +1,15 @@
+//Program for While Loop in Character
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	char ch = 'a';
+	cout<<"While Condition with Character "<<endl;
+	while(ch)
+	{
+		cout<<"All the Alphabets are present here :"<<ch <<endl;
+		ch++;
+	}
+	return 0 ;
+	
+}
