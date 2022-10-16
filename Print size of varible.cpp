eedda2aa8588e2fe1815c3352e9fd38615aae7b9
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-10-16 22:45:29 +0530

    Print size of VAriable

---
+++ Print size of varible.cpp
@@ -0,0 +1,12 @@
+//Program for print size  of a Variable
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	cout<<"Size fo char "<< sizeof(char)<<"bytes"<<endl;
+	cout<<"Size of int "<<sizeof(int)<<"bytes"<<endl;
+	cout<<"Size of float "<<sizeof(float )<<"bytes"<<endl;
+	cout<<"Size of double"<<sizeof(double)<<"bytes"<<endl;
+	
+	return 0;
+}
