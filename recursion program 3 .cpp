56d5598ee5958f49308c3096263eb070817d857f
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-10-16 22:51:24 +0530

    recursion progarm 3

---
+++ recursion program 3 .cpp
@@ -0,0 +1,21 @@
+//Progarm for Recursion
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	void count (int n)
+	cout<< <<n<<endl;
+	cout<< << d<<endl;
+	
+	d++;
+	if(n>1)
+	count (n-1);
+	cout<<" "<<d<<endl;
+	
+}
+{
+	count (3);
+	
+	return 0;
+}
+
