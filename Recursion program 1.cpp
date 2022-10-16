df8d1a56722189b1ced1b6d1f661498d30d78a00
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-10-16 22:50:09 +0530

    REcursion progasrm 1

---
+++ Recursion program 1.cpp
@@ -0,0 +1,20 @@
+//Progarm for Recursion 
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int sum (int x)
+	int s=0;
+	if(x==1)
+	{
+		return x;
+	}
+	s=x+sum(x-1);
+	return S;
+}
+int a ;
+a =sum(S);
+cout<<a<<endl;
+
+return 0;
+}
