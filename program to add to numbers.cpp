f0d56023a1bc10f95a0d374e664e945572d5a748
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-10-16 23:05:24 +0530

    The sum of two numbers

---
+++ program to add to numbers.cpp
@@ -0,0 +1,12 @@
+//Progarm for add of two numbers
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int a , b;
+	cout<<"Enter a numbers"<<endl;
+	cin>> a>>b;
+	cout<< "The sum is "<< a+b;
+	
+	return 0;
+}
