562c5c0de4dd260c50eefd19686dc0a853892311
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-10-16 22:53:09 +0530

    tail and non-tail recursion

---
+++ Tail and NOn tail recursiion .cpp
@@ -0,0 +1,17 @@
+//Program for Tail and non-tail recursion
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	cout<<int a;
+	if(a<1)
+	return 
+	else 
+	cout<< a<<endl;
+	cout<<a/2<<endl;
+}
+cout<<"Enter the Code"<<endl;
+cout<<(10)<<endl;
+
+return 0;
+}
