d859d40d26e46bdaf3e78aa278750e36eef3a062
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-10-18 15:26:56 +0530

    Pointer

---
+++ Pointer.cpp
@@ -0,0 +1,18 @@
+//Program for pointer in C++
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int t1;
+	int  a =100 ;
+	int *p1;
+	int **p2;
+	t1=&a;
+	p2=&p1;
+	cout<<"Address of A "<< &a<<endl;
+	cout<<"Address of A "<< ptr1<<endl;
+	cout<<"VAlue of A "<< *ptr1<<endl;
+	cout<<"Value of A "<< ** ptr2<<endl;
+	
+	return 0;
+}
