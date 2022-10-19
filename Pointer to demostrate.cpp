9d6087ba91802c68a1c75775b9cf0e53b9f33f2b
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-10-19 12:45:03 +0530

    Pointer to demostrate

---
+++ Pointer to demostrate.cpp
@@ -0,0 +1,11 @@
+//Progarm to demostrate the pointer
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int x=76;
+	cout<<"Value of x ="<<x<<endl;
+	cout<<"Address of x "<< &x<<endl;
+	
+	return 0;
+}
