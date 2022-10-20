8bcf424f713b0b4aad48be3a29a261cd50896fad
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-10-20 11:56:55 +0530

    template for int and float

---
+++ Template for int and float 1.cpp
@@ -0,0 +1,15 @@
+ //Program for Template
+ #include<bits/stdc++.h>
+ using namespace std;
+ 
+ template <class t>
+ t sum (t a, t b )
+ {
+ 	return a+b;
+ }
+ main()
+ {
+ 	cout<<"Int sum ="<<sum(3,5)<<endl;
+ 	cout<<"Float sum ="<< fl(1.2, 4.5)<<endl;
+ 	
+ }
