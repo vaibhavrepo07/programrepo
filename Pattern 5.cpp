693f98ebd22984c4654c59711bf8a8e7d612bc60
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-10-21 16:14:02 +0530

    Pattern 5

---
+++ Pattern 5.cpp
@@ -0,0 +1,16 @@
+//Progarm for Pattern 5 to print 5 to 1 in reverse order
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	cout<<"Enter the coding with 5 to 1 in revese order"<<endl;
+	for(int i=5;i>=1;i--)
+	{
+		for(int j=5;j>=1;j--)
+		{
+			cout<<j;
+		}
+		cout<<endl;
+	}
+	return 0;
+}
