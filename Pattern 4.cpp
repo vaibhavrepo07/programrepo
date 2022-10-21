07a3d13a4aa3df949c9e9a6920ee223faf5cff6c
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-10-21 16:12:20 +0530

    Pattern 4

---
+++ Pattern 4.cpp
@@ -0,0 +1,16 @@
+//Program for Pattern 4 print from Increasing to Decreasing
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	cout<<"Pattern 4"<<endl;
+	for(int i=5;i>=5;i--)
+	{
+		for(int j=1;j<=5;j++)
+		{
+			cout<<i
+		}
+		cout<<endl;
+	}
+	return 0 ;
+}
