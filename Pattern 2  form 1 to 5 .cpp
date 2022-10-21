df18fec678c8462773a0582d672ce38f85bcb522
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-10-21 16:07:00 +0530

    Pattern 2

---
+++ Pattern 2  form 1 to 5 .cpp
@@ -0,0 +1,16 @@
+//Progarm for Numeric Constant from 1 to 5
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	cout<<"Coding with Numeric Constant from 1 to 5"<<endl;
+	for(int i=1;i<=5;i++)
+	{
+		for(int j=1;j<=5;j++)
+		{
+			cout<<i <<j ;
+		}
+		cout<<endl;
+	}
+	return 0;
+}
