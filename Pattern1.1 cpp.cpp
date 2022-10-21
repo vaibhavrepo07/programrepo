9ef397b57a6474164dccbba1e8984e2ddfb98351
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-10-21 16:14:50 +0530

    Pattern1.1

---
+++ Pattern1.1 cpp.cpp
@@ -0,0 +1,16 @@
+//Progarm for Pattern Ampersand 
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	cout<<"Program for pattern in & "<<endl;
+	for(int i=1 ;i<=7;i++)
+	{
+		for(int j=1;j<=7;j++)
+		{
+			cout<<"&";
+		}
+		cout<<endl;
+	}
+	return 0 ;
+}
