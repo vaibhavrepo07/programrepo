e371c962aa5cbebfc0f90303d79d8677db190b5f
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-10-21 16:16:38 +0530

    Pattern 3

---
+++ Pattern3.cpp
@@ -0,0 +1,16 @@
+//Progarm for Pattern 3 
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	cout<<"Enter the Pattern "<<endl;
+	for(int i=1;i<=7;i++)
+	{
+		for(int j=1;j<=7;j++)
+		{
+			cout<<i *j<<endl;
+		}
+		cout<<endl;
+	}
+	return 0;
+}
