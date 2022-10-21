ff8991331f3f7c5156acd055993fd7695669df29
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-10-21 16:11:33 +0530

    Pattern 4.1

---
+++ Pattern 4.1 .cpp
@@ -0,0 +1,16 @@
+//Program for Pattern Numeric constant from 5 to 1
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	cout<<"Coding with Pattern of Numeric constant "<<endl;
+	for(int i=5;i>=1 ;i--)
+	{
+		for(int j=1;j<=5;j++)
+		{
+			cout<<i;
+		}
+		cout<<endl;
+	}
+	return 0;
+}
