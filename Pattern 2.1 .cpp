ee6de17cae33eacd6e4b342e1db0a92102b5c493
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-10-21 16:09:28 +0530

    Pattern 2.1

---
+++ Pattern 2.1 .cpp
@@ -0,0 +1,17 @@
+//Program for Numeric constant 
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	
+	cout<<"Coding with Numeric Constant form 1 to 5"<<endl;
+	for(int i=1;i<=5;i++)
+	{
+		for(int j=1 ;j<=5;i++)
+		{
+			cout<<i;
+		}
+		cout<<endl;
+	}
+	return 0 ;
+}
