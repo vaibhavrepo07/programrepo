547109a8f8e564c885d8a216232bb926a1bcdb59
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-10-21 16:15:39 +0530

    Pattern 1.2 Hash

---
+++ Pattern1.2 Hash .cpp
@@ -0,0 +1,16 @@
+//Progarm for Hash pattern 
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	cout<<"Coding with Hash Pattern "<<endl;
+	for(int i=1;i<=10;i++)
+	{
+		for(int j=1;j<=10 ;j++)
+		{
+			cout<<"#";
+		}
+		cout<<endl;
+	}
+	return 0;
+}
