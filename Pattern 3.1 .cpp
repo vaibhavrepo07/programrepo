06a4bd1696f2919f3f7efe568e232008f29746e2
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-10-21 16:10:37 +0530

    Pattern 3.1

---
+++ Pattern 3.1 .cpp
@@ -0,0 +1,16 @@
+//Progarm for Pattern 
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	cout<<"Progarm for Print the Number in Sequence from 1 to 5"<<endl;
+	for(int i=1;i<=6;i++)
+	{
+		for(int j=1;j<=6;j++)
+		{
+			cout<<j;
+		}
+		cout<<endl;
+	}
+	return 0;
+}
