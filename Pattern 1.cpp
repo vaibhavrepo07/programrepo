64e38e41e40e904c2d886c29dee495bda65c0e97
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-10-21 16:05:00 +0530

    Pattern 1

---
+++ Pattern 1.cpp
@@ -0,0 +1,16 @@
+//Progarm for Pattern 1
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	cout<<"Coding with Pattern 1"<<endl;
+	for(int i=1 ;i<=5; i++)
+	{
+		for(int j=1;j<=5;j++)
+		{
+			cout<<"*";
+		}
+		cout<<endl;
+	}
+	return 0;
+}
