d4c11339af16607cb8f900e063eb938603054529
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-10-22 12:30:14 +0530

    Template initial

---
+++ Template Initial.cpp
@@ -0,0 +1,17 @@
+//Progarm for Function Overloading 
+#include<bits/stdc++.h>
+using namespace std;
+int fun(int x ,int y)
+{
+	return x+y;
+}
+float fun1(float x,float y)
+{
+	return x+y;
+}
+ int main()
+ {
+ 	cout<<"Print Template"<<endl;
+ 	
+ 	return 0;
+ }
