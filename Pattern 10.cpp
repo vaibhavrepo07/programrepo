3e8aa6ee0a94bcb4ae1e8c63138305b0691a5ab4
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-10-23 18:26:06 +0530

    Pattern 10

---
+++ Pattern 10.cpp
@@ -0,0 +1,15 @@
+//Program for Pattern 10
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	for(int i=1;i<=5;i++)
+	{
+		for(int j=1;j<=3;j++)
+		{
+			cout<<j<<" "<<i<<" ";
+		}
+		cout<<endl;
+	}
+	return 0;
+}
