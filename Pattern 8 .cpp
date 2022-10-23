1530ec97c6ad1c45d8d71b77b4750f102cfa9d08
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-10-23 18:23:35 +0530

    Pattern 8

--- Pattern 8 .cpp
+++ Pattern 8 .cpp
@@ -1,13 +1,13 @@
-//Program for Pattern 8 in cpp
+//Program for Pattern 8 
 #include<bits/stdc++.h>
 using namespace std;
 int main()
 {
-	int n = 5;
-	int x = 2;
-	for(int i=1 ;i<=n ;i++)
+	int n=5;
+	int x=2;
+	for(int i=1;i<=n;i++)
 	{
-		for(int j=1 ;j<=n ;j++)
+		for(int j=1;j<=n;j++)
 		{
 			cout<<x<<" ";
 			x+=2;
