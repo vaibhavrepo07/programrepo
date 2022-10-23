2e8f24c080e01c3fe3789def0a241f7ff4f048f7
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-10-23 18:30:45 +0530

    Pattern 17

---
+++ Pattern 17.cpp
@@ -0,0 +1,17 @@
+//Program for pattern 17
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n=5;
+	for(int i=1;i<=n;i++)
+	{
+		for(int j=1;j<=n;j++)
+		{
+			cout<<(2*(i+j))-3<<" ";
+			}
+			cout<<endl;	
+			
+			}
+			return 0;
+}
