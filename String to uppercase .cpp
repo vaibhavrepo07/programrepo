12525c59c41335a019a1506e0548220119baa91d
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-10-24 17:51:22 +0530

    String to Uppercase

---
+++ String to uppercase .cpp
@@ -0,0 +1,12 @@
+//Program for String to upper chars
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	char str[20]="happy birthday";
+	cout<<"Original string is :"<<str<<endl;
+	strupr(str);
+	cout<<"String after strupr"<<str<<endl;
+	
+	return 0;
+}
