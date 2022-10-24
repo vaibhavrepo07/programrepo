af5ff2e56cbef23eb259366c5045c7ad697d2230
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-10-24 17:47:25 +0530

    String Reverse

---
+++ String Reverse.cpp
@@ -0,0 +1,13 @@
+//Program for String Compare
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	char str[20]="Hey Friend";
+	cout<<"Original String"<<str<<endl;
+	strrev(str);
+	cout<<"String after strrev"<<str<<endl;
+	
+	return 0;
+}
