5229327d0b72ad2ddeab9c5cb367bda0b3cac4c5
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-10-24 17:49:07 +0530

    String strlwr

---
+++ string strlwr (to lower chars).cpp
@@ -0,0 +1,12 @@
+//Program for strlwr 
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	char str[20]="HELLO WORLD";
+	cout<<"Original String is :"<<str<<endl;
+	strlwr(str);
+	cout<<"String after strlwr:"<<str <<endl;
+	
+	return 0;
+}
