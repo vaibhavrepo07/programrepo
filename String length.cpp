acc7517a05485c65b07413cbbf05f83afa8005ce
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-10-24 17:46:33 +0530

    String Length

---
+++ String length.cpp
@@ -0,0 +1,14 @@
+//Progarm for String Length 
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	char s[20];
+	int i;
+	cout<<"Enter the String "<<endl;
+	cin.getline(s,20);
+	i=strlen(s);
+	cout<<"The Length of the String is"<<i<<endl;
+	
+	return 0;
+}
