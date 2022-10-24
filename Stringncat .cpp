bf19eb659fa2eac20e77b81f7529e42626c74d5a
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-10-24 17:52:12 +0530

    Stringcat

---
+++ Stringncat .cpp
@@ -0,0 +1,21 @@
+//Program for Strncat
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	char s1[20];
+	char s2[20];
+	unsigned int len;
+	
+	cout<<"Enter the First String "<<endl;
+	cin.getline(s1, 20);
+	cout<<"Enter the Second String "<<endl;
+	cin.getline(s2,20);
+	
+	//Compute how many characters will actually fit 
+	len=19-strlen(s1);
+	strncat (s1,s2,len);
+	cout<<s1<<endl;
+	
+	return 0;
+}
