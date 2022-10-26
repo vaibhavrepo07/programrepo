57a0d3f0f2314c72e841d818cb85251192950771
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-10-26 14:17:15 +0530

    CAscading insertionn and extraction

---
+++ Cascading Insertion and Extraction,cpp.cpp
@@ -0,0 +1,16 @@
+//Program for Cascading Insertion and Extration
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int num1,num2,sum;
+	
+	cout<<"Enter two Number"<<endl;
+	cin>> num1 >> num2;
+	
+	sum=num1+num2;
+	
+	cout<<num1<<"+"<<num2<<"="<<sum<<endl;
+	
+	return 0;
+}
