5d6cb27867c234743be30e8dc0b88f3ab16f77e9
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-10-26 14:29:03 +0530

    Variable 123

---
+++ Variable Decleraion.cpp
@@ -0,0 +1,15 @@
+//Progarm for Variable declaration and Assignment
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int length ; //Variable Declaration
+	
+	length = 10;   //Assignment
+	
+	cout<<"The Length is :"<<endl;
+	cout<<length<<endl;
+	
+	return 0;
+}
