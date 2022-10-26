8fdf403aa3cb82ce4a79876b200bc23ebfd7d1bf
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-10-26 14:22:01 +0530

    endl

---
+++ endl.cpp
@@ -0,0 +1,14 @@
+//Progarm for endl 
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+	int a=12, b=123, c=1234;
+	
+	cout<<"Numbers"<< endl;
+	cout<<a <<endl;
+	cout<<b <<endl;
+	cout<<c <<endl;
+	
+	return 0;
+}
