b0cf8644ecac25d52ec0a2239298c4220f8ccebe
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-10-26 14:23:42 +0530

    Multiple Comment

---
+++ Multiple Comment .cpp
@@ -0,0 +1,15 @@
+//Progaram for multiple comment 
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	float r , h ;
+	cout<<"Enter radius and Height of Cylinder"<<endl;
+	cin>>r>>h;
+	
+	float v= 3.14 *r *r *h;
+	
+	cout<<"Volume of Cylinder is = "<<v<<endl;
+	
+	return 0;
+}
