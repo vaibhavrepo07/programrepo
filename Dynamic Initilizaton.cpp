6905fe66e02ece4b45175a5ced22577d930b2c3d
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-10-26 14:21:12 +0530

    Dynamic Initialization

---
+++ Dynamic Initilizaton.cpp
@@ -0,0 +1,15 @@
+//Program for Dynamic Initilization
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	double radius = 10.0 , heigth = 10.0 ;
+	
+	//dynamically initialization of volume 
+	double volume = 3.1415 * radius * radius * height ;
+	
+	cout<<"Volume is :::"<<volume <<endl;
+	
+	return 0;
+}
