34118beacae277afec80a807dca5b9b60a04d5ab
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-10-26 14:27:30 +0530

    Swap

---
+++ swap.cpp
@@ -0,0 +1,19 @@
+//Progarm for swap two number
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int n1,n2,t;
+	
+	cout<<"Enter two number"<<endl;
+	
+	cin>> n1>> n2;
+	
+	t=n1;
+	n1=n2;
+	
+	cout<<n1<<n2<<endl;
+	
+	return 0;
+}
