bce9681c93abcbc23c255e2ef1dafe4ffc4c868b
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-10-27 11:04:12 +0530

    Program for Constant and Variable Definition

---
+++ Progarm for Constant and Variable Definition.cpp
@@ -0,0 +1,17 @@
+//Program for Constant and Variable Definition
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int sin,cos,theta;
+	const float e=2.71;
+	
+	cout<<"Find the value of sin,cos,theta"<<endl;
+	cin>>sin>>cos;
+	theta=sin*sin + cos*cos ;
+	cout<<"The solution of sin square theta and cos square theta is ="<<theta<<endl;
+	
+	cout<<"The Golden value is ="<< e<<endl;
+	
+	return 0;
+}
