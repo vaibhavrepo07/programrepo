837b4ba3a9832eb9aa7a232c98bc1e2c5b2a03f2
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-10-27 11:02:56 +0530

    Getch()andGetche()

---
+++ Getch()and Getche().cpp
@@ -0,0 +1,17 @@
+//Program for getch and getche 
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int ch ;
+	
+	cout<<"The value of Getch"<<endl;
+	ch=getch();
+	cout<<"ch"<<ch<<endl;
+	
+	cout<<"The value of Getche()"<<endl;
+	ch=getche();
+	cout<<"ch"<<ch<<endl;
+	
+	return 0;
+}
