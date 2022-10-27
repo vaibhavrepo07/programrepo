6f4863ec25da6bc778d321ff728949a8052ba7d7
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-10-27 10:56:02 +0530

    Assign the Value to next variable

---
+++ Assign the value to next varible .cpp
@@ -0,0 +1,17 @@
+//Program for Variable Declaration and Variable Assignment 
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int  a; 
+	int b = 10;
+	
+	cout<<"The value of a "<<endl;
+	cout<<"The value  of b "<<endl;
+	
+	a=b;
+	cout<<b<<endl;
+	
+	return 0;
+	
+}
