dcbef821aa0b31f34b6bf1ccb89b2c41c01a5bae
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-10-27 10:58:39 +0530

    Dynamically Iitialization

---
+++ Dynamically Initialization.cpp
@@ -0,0 +1,16 @@
+//Program for Dynamic Initialization 
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int a=10;int r;
+	int b;
+	cout<<"Program for Dynamic Initialization"<<endl;
+	cin>>r;
+	
+	cout<<" "<<a<<endl;
+	cout<<" "<<r<<endl;
+	b=4*3.14*r*r;
+	cout<<" "<<b<<endl;
+	return 0;
+}
