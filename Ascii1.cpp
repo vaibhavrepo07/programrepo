308920402e4c4b96e3a6e9fcf85f526077927efb
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-10-27 10:54:50 +0530

    Ascii1

---
+++ Ascii1.cpp
@@ -0,0 +1,13 @@
+//Program for ASCII character
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	char ch;
+	cout<<"The value of ASCII "<<endl;
+	cin>>ch;
+	cout<<"The Character"<<ch<<"The value of Integer is"<< int(ch)<<endl;
+	
+	return 0;
+	
+}
