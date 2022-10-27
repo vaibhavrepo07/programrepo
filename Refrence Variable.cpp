44a0d740ca33bd575d971c1eaa659b9ecbddece8
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-10-27 11:06:44 +0530

    Refrence VAriable

---
+++ Refrence Variable.cpp
@@ -0,0 +1,20 @@
+//Progarm for Refrence variable 
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	
+	
+	int &x;
+	float &y;
+	
+	
+	cout<<"The value of a is = "<<endl;
+	cout<<"The Refrence value of a is ="<<x<<endl;
+	
+
+	cout<<"The value of b is = "<<endl;
+	cout<<"The Refrence value of b is ="<<y<<endl;
+	
+	return 0;
+}
