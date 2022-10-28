22ee9067d8151a1082351b24fb6f51d4be778113
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-10-28 15:38:22 +0530

    Assignment Operator

---
+++ Assignment Operator.cpp
@@ -0,0 +1,13 @@
+//Program for Assignment Operator 
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int a , b ,c ;
+	a=b=c=10;
+	
+	cout<<a<<b<<endl;
+	cout<<c<<b<<endl;
+	
+	return 0;
+}
