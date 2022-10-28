ffc53763c4f5745ddbeb65d5f6814fdee4b79aea
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-10-28 15:41:33 +0530

    pre and post decrement

---
+++ Pre and Post decrement .cpp
@@ -0,0 +1,16 @@
+//Program for  Prefix and Postfix operator 
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	
+	int a=10,b=20,c;
+    
+    c=(--a)+(--b);
+    
+    cout<<a<<endl;
+    cout<<b<<endl;
+    cout<<c<<endl;
+    
+    return 0;
+}
