96542a8eb369657544cb09fd625ce9c7a5f1e59f
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-10-28 15:39:28 +0530

    Bitwise Operator

---
+++ Bitwise Operator .cpp
@@ -0,0 +1,17 @@
+//Program for Bitwise Operator
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int a=2,b=4,c;
+	
+	cout<< a;
+	cout<<b;
+	
+	c=a&b;
+	
+	cout<<c<<endl;
+	
+	return 0;
+}
