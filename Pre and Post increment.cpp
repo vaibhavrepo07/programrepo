aca0ce252b8fffa805b91cf6d6606041d86f9deb
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-10-28 15:42:37 +0530

    Pre and Post Increment

---
+++ Pre and Post increment.cpp
@@ -0,0 +1,14 @@
+//Program for Perfix and Postfix Increment
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int a =10 ,b=20, c;
+	c=(++a)+(++b);
+	
+	cout<<a<<endl;
+	cout<<b<<endl;
+	cout<<c<<endl;
+	
+	return 0 ;
+}
