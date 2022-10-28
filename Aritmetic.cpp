820b68128824c9f7ac5a02da970324a4bf192881
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-10-28 15:36:13 +0530

    Arithmetic Operator

---
+++ Aritmetic.cpp
@@ -0,0 +1,14 @@
+//Program for Arithmetic 
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int a,b,c;
+	cin>>a>>b;
+	cout<<"Enter two Number"<<endl;
+	c=a+b;
+	cout<<"The value of a + b is ="<<c<<endl;
+	
+	return 0;
+	
+}
