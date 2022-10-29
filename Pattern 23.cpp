b0c52c1a68119b61030bb0792587f4aa3154425f
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-10-29 17:20:44 +0530

    Pattern 23

---
+++ Pattern 23.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n=5;
+	
+	for(int i;i<=n ;i++)
+	{
+		for(int j=1;j<=i;j++)
+		{
+			cout<<j*2<<" ";
+		}
+		cout<<endl;
+		
+	}
+	return 0;
+}
