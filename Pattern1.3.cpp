80562534e4da13a16ef6c049757b138d774033cf
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-10-29 17:07:31 +0530

    Pattern1.3

---
+++ Pattern1.3.cpp
@@ -0,0 +1,26 @@
+//Program for Pattern 1.2
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n=5;
+	int i,j,s;
+	for(i=1;i<=n;i++)
+	{
+		for(s=n-1;s>=i;s--)
+		{
+			cout<<" ";
+		}
+		for(j=1;j<=i;j++)
+		{
+			if(i%2==0)
+			{
+				cout<<(char)(i+64); // to get character
+			}
+			else
+			cout<<i<<endl;
+		}
+		cout<<endl;
+	}
+	return 0;
+}
