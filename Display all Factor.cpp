c95629e253ffa54a5117a51f0e550602bba67f7d
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-10-30 12:03:40 +0530

    Display all factor

---
+++ Display all Factor.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int n , i;
+	cout<<"Enter a positive integer"<<endl;
+	cin>>  n;
+	
+	cout<<"Factors of "<<n<<"are"<<endl;
+	for(i=1 ;i<=n ;i++)
+	{
+	 cout<<i<<endl;
+	 
+	}
+	return 0;
+}
