ae70096854947a309ca9959fb1ad00bf5ef9ac4d
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-10-31 15:59:21 +0530

    Pattern Star

---
+++ Pattern star.cpp
@@ -0,0 +1,22 @@
+//Program for Pattern 
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int i, j, k;
+	int p_height =5;
+	
+	for(int i=p_height ;i>=1;i--)
+	{
+		for(int k=p_height-1;k>=i;k--)
+		{
+			cout<<" ";
+		}
+		for(int j=i; j>=1;j--)
+		{
+			cout<<"*"; 
+		}
+		cout<<endl;
+	}
+	return 0;
+}
