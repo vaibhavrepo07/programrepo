e204cfa63caed747ba110af7b5b92bcc1d4f595a
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-10-31 22:07:25 +0530

    Volume of Hemisphere

---
+++ Volume of Hemisphere.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	float radius;
+	float volume ;
+	
+	cout<<"Enter radius of the Hemisphere"<<endl;
+	cin>>radius;
+	
+	volume=(2*3.14*radius)/3;
+	
+	cout<<"Volume of Hemisphere is "<<volume <<endl;
+	
+	return 0;
+}
