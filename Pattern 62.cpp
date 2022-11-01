bada575109b41258a9f49560fa61d71c170d13b2
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-01 22:11:23 +0530

    pattern 62

---
+++ Pattern 62.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int n=5;
+	
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<=i;j++)
+		{
+			cout<<(j%2)<<" ";
+		}
+		cout<<endl;
+	}
+	return 0;
+}
