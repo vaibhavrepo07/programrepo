862de4e74e0396e5f420b5c1e31f55109c8eacb0
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-02 21:01:13 +0530

    SimpleGoto

---
+++ SimpleGoto.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int i;
+	i=1;
+
+   Nils:
+   	cout<<" "<<i<<endl;
+   	
+   	i++;
+   	if(i<=10)
+   	{
+   		goto Nils;
+	   }
+	   return 0;
+}
