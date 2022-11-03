9b82e28a36a197014bc0819c0e98ed7ba49e9842
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-03 22:20:40 +0530

    Programf6

---
+++ Programf6.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int count ;
+	if(count ==0)
+	{
+		cout<<"this is  a  prime number"<<endl;
+		
+	}
+	else 
+	{
+		cout<<"This is not  a prime number"<<endl;
+		
+		
+	}
+	return 0;
+	
+}
