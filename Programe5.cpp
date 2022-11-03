9c7b619935296e2ba525428885628c06646a6d86
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-03 22:19:22 +0530

    Programe5

---
+++ Programe5.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int num, i, count=0;
+	
+	cout<<"Enter a Number"<<endl;
+	
+	cin>> num;
+	
+	for(i=2;i<num;i++)
+	{
+		count ++;
+		
+		break;
+	}
+	return 0;
+	
+}
