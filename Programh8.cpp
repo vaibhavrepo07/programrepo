4fc43292d87d96507bc03d9372b3d5773f36c3f7
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-03 22:22:34 +0530

    Programh8

---
+++ Programh8.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	float a, b;
+	cout<<"Enter any two numbers"<<endl;
+	
+	cin>>a >> b;
+	
+	cout<<"Largest among given two number"<<a+b;
+	
+	return 0;
+	
+}
