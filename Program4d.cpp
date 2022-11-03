690f2282d8664e105219d46da5e188219133f8aa
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-03 22:17:33 +0530

    program4d

---
+++ Program4d.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int x,y ;
+	
+	cout<<setw(5)<<setw(5)<<x<<endl;
+	cout<<y<<endl;
+	
+	cout<<setw(10)<<setw(5)<<x+y<<endl;
+	
+	return 0;
+	
+}
