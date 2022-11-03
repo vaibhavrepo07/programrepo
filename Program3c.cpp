dbfddd7b13ad365a0940a76960d1af6380c192b8
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-03 22:16:52 +0530

    Program3c

---
+++ Program3c.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int x, y;
+	cout<<"Enter two integer numbers"<<endl;
+	
+	cin>>x;
+	
+	cin>>y;
+	
+	cout<<"Number are ";
+	
+	cout<<y<<endl;
+	
+	return 0;
+}
