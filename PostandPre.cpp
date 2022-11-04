cbd22386524b010c7a35583f250bf22e3963f1bd
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-04 11:09:50 +0530

    PostandPre

---
+++ PostandPre.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int a ;
+	
+	cin>> a;
+	cout<<"Coding with Post and Pre Decrement"<<endl;
+	cout<<"PostDecrement "<<endl;
+	cout<<"PreDecrement  a--"<<a--<<endl;
+	
+	cout<<"Coding with PreDecrement"<<endl;
+	cout<<"PreDecrement"<< --a<<endl;
+	
+	return 0;
+	
+}
