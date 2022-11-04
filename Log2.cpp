7dfddd77e2e3c66d78d9c9dc2e002e9f0cca1b9d
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-04 11:00:09 +0530

    Log2

---
+++ Log2.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	cout<<"Logical AND Operator"<<endl;
+	cout<<((10<20)&&(30<40));
+	cout<<((10<30)&&(30>40));
+	
+	cout<<"Logical OR Operator"<<endl;
+	cout<<((10<=30)||(30>89));
+	cout<<((89>34)||(28>=87));
+	
+	cout<<"Logical NOT Operator"<<endl;
+	cout<<(!(20<49));
+	cout<<(!(34<=78));
+	
+	return 0;
+	
+}
