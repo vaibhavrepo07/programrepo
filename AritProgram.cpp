4a775ba3e41aeab6ed1ce7fdd3258e4beaf98402
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-04 10:51:31 +0530

    AritProgram

---
+++ AritProgram.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int a,b,res;
+	
+	cout<<"Enter two Numbers"<<endl;
+	cin>>a>>b;
+	
+	res=a+b;
+	cout<<a<<"+"<<b<<res<<endl;
+	
+	res=a*b;
+	cout<<a<<"*"<<b<<res<<endl;
+	
+	res=a/b;
+	cout<<a<<"/"<<b<<res<<endl;
+	
+	res=a-b;
+	cout<<a<<"-"<<b<<res<<endl;
+	
+	res=a%b;
+	cout<<a<<"%"<<b<<res<<endl;
+	
+	return 0;
+}
