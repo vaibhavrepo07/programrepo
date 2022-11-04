4271e2bae3f9bd4e4b56aaf5b621c25e3b39b2c2
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-04 10:59:23 +0530

    Log1

---
+++ Log1.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int a,b;
+	cout<<"Enter the Logical Operator"<<endl;
+	cin>>a>>b;
+	
+	cout<<(a&&b)<<endl;
+	cout<<(a||b)<<endl;
+	cout<<!(b)<<endl;
+	
+	return 0;
+	
+}
