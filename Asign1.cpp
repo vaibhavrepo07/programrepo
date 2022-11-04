187225b928c8cfc5b4b4a0d127448442f8b327e8
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-04 10:53:59 +0530

    asign1

---
+++ Asign1.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int num=30;
+	
+	cout<<"Num is"<<num;
+	num+=10;
+	
+	cout<<"Num+=10"<<num<<endl;
+	num-=10;
+	
+	cout<<"Num*=10"<<num<<endl;
+	num*=10;
+	
+	return 0;
+	
+}
