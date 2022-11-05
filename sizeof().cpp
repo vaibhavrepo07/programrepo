1e047f8c2a461143ac796df61268b8b0adc07a25
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-05 14:25:10 +0530

    sizeof()

---
+++ sizeof().cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	cout<<"The size of an int is:"<<endl;
+	cout<<sizeof(int )<<"bytes"<<endl;
+	
+	cout<<"The size of a short int "<<endl;
+	cout<<sizeof(short)<<"bytes"<<endl;
+	
+	cout<<"The size of Char is "<<endl;
+	cout<<sizeof(char)<<"Bytes"<<endl;
+	
+	cout<<"The size of float is "<<endl;
+	cout<<sizeof(float)<<"Bytes"<<endl;
+	
+	cout<<"The size of  a double "<<endl;
+	cout<<sizeof(double)<<"Bytes"<<endl;
+	
+	return 0;
+}
