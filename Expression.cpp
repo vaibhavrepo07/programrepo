7580a98e2d9e1d8b3a5e9a1ff76908ccc94620d5
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-05 14:18:30 +0530

    Expression

---
+++ Expression.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int w,x,y,z,p;
+	
+	cout<<"Enter 4 Integers"<<endl;
+	cin>>w>>x>>y>>z;
+	
+	p=(w+x)/(y-z);
+	
+	cout<<"Value of P ="<<p<<endl;
+	
+	return 0;
+	
+}
