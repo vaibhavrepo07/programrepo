1dadb436dd8c1accd5c80459956b359bef540d3b
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-05 14:27:05 +0530

    UseComma

---
+++ UseComma.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int num ,sq, cube;
+	num = 20 ;
+	
+	sq=(num*num) , cube=(num*num*num);
+	
+	cout<<"The Square of "<< num <<sq<<endl;
+	cout<<"The Square of "<< num <<cube<<endl;
+ 
+    return 0;
+    
+ }
