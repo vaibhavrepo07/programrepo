9ae784bf1c30586a361d6344f5cdf0e6f6aaafec
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-05 14:23:28 +0530

    Rel

---
+++ Rel.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int  main()
+{
+	int n ;
+	
+	cout<<"Enter any Number"<<endl;
+    cin>>n;
+    
+    cout<<(n<=10)<<endl;
+    cout<<(n<10)<<endl;
+    cout<<(n>10)<<endl;
+    cout<<(n>=10)<<endl;
+    cout<<(n==10)<<endl;
+    cout<<(n!=10)<<endl;
+    
+    return 0;
+    
+    
+}
