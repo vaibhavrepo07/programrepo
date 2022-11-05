1433355f9d8267319dd0666cb9ab3bbcabe4421f
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-05 14:24:25 +0530

    Rel1

---
+++ Rel1.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int a , b;
+	int c;
+	
+	cout<<"Coding with Relation Operator"<<endl;
+	cin>>a >> b;
+	
+	c=(a<b);
+	c=(a<=b);
+	c=(a>b);
+	c=(a>=b);
+	c=(a==b);
+	c=(a!=b);
+	cout<<c<<endl;
+	
+	return 0;
+}
