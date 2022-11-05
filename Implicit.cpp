a518039b6ca824a6871b60b0facc8cf92f24bde0
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-05 14:19:44 +0530

    Implicit

---
+++ Implicit.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int a=10, b=30;
+	float c=49.30;
+	double d=30L, e;
+	
+	e=((a*b)*c)+d;
+	
+	cout<<"The value of Implicit "<<e<<endl;
+	
+	return 0;
+	
+}
