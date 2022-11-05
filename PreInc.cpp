913c670a5696880d177cbc3be8b235cfe00fcd81
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-05 11:33:37 +0530

    ProgramForSummation

---
+++ PreInc.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+
+{
+	int a;
+	cout<<"The value of PreIncrement is ="<<++a<<endl;
+	cout<<"The value of PostIncrement is ="<<a++<<endl;
+	
+	return 0;
+	
+}
---
+++ Program for Summation.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int a , b , c;
+	cout<<"Coding with Summation "<<endl;
+	
+	cin>> a>> b;
+	
+	c=a+b;
+	
+	cout<<"The Summation of Two numeber is "<< c<<endl;
+	
+	return 0 ;
+	
+}
