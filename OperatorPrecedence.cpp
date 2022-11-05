14d5091421125c6b2549f8c0247fbf5383e30c0c
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-05 14:20:50 +0530

    OperatorPrecedence

---
+++ OperatorPrecedence.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	float result;
+	
+	result =1.0+3.2*4.9/3.2;
+	cout<<" "<<result <<endl;
+	
+	result = 4.5/ 2/3+4.3;
+	cout<<" "<<result<<endl;
+	
+	result = (3.2+4.4)/4;
+	cout<<" "<<result<<endl;
+	
+	result = (3.2*5.3/5.2)+4.2;
+	cout<<" "<<result <<endl;
+	
+	return 0;
+	
+}
