e97128b3cb2db2dd7f67cea2266181d4ba1f68f4
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-05 14:26:16 +0530

    TernaryProgram

---
+++ Ternaryprogram.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int a,b, val;
+	cout<<"Coding with Ternary Operator"<<endl;
+	cin>>a >> b;
+	
+	val = (a>b)?a:b;
+	
+	cout<<"The value of Ternary Operator "<<val << endl;
+	
+	return 0;
+	
+	
+}
