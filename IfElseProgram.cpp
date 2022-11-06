4a74300907c7db919d8d24adc9d052f7b45d5e67
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-06 11:34:21 +0530

    IfElseProgram

---
+++ IfElseProgram.cpp
@@ -0,0 +1,22 @@
+//Program for If Else Statement
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int a , b ;
+	cin>> a >> b;
+	
+	cout<<"Let's check the If else statement "<<endl;
+	if(a>b)
+	{
+		cout<<"A is Greater than B"<<endl;
+		
+	}
+	else
+	{
+		cout<<"B is Greater than A"<<endl;
+	}
+	
+	return 0;
+}
