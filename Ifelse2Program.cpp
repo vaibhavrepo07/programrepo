60126b1912868374938182c6c5cc9e12e0ab5330
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-06 11:32:39 +0530

    IFElse2Program

---
+++ Ifelse2Program.cpp
@@ -0,0 +1,21 @@
+//Program for IfElse Statement
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int  n ;
+	cin>>  n ;
+	
+	cout<<"Coding with If else Statement"<<endl;
+	if(n>100)
+	{
+		cout<<"The Number is Greater than 100"<<endl;
+	}
+	else
+	{
+		cout<<"The Number is Smaller than 100 "<<endl;
+	}
+	
+	return 0;
+	
+}
