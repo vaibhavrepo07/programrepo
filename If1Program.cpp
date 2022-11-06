f5cf604c98913b435a1d6f44c4190fee96bda202
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-06 11:29:20 +0530

    IF1Program

---
+++ If1Program.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using  namespace std;
+int main()
+{
+	int a  , max ;
+	cin>> a>> max;
+	
+	cout<<"The Statement of If is true or Not "<<endl;
+	if(max>a)
+	{
+		cout<<"The True Statement here "<<endl;	
+	}
+	
+	
+	return 0;	
+}
