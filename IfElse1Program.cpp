9c459b4e9ca5c316298d17a0adc5bc37848813cb
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-06 11:31:16 +0530

    IFElseProgram

---
+++ IfElse1Program.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int x=0, y=1;
+	cout<<"Lets see by Logical AND"<<endl;
+	
+	if(x&&y)
+	{
+		cout<<"The value of x is Appropriate "<<endl;
+	}
+	else
+	{
+		cout<<"The value of y is Satisfied "<<endl;
+	}
+	
+	return 0;
+}
