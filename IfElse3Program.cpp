4f80fedcd0f997b60644b250eef5fbc0bb102175
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-06 11:33:33 +0530

    IFElse3Program

---
+++ IfElse3Program.cpp
@@ -0,0 +1,20 @@
+//Program for Maximum of Two Numbers using if else statement
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int a=20 , b=30;
+	
+	cout<<"Let's check the Value of A and B"<<endl;
+	
+	if(a>b)
+	{
+		cout<<"A is Greater than B "<<endl;
+	}
+	else
+	{
+		cout<<"B is Greater than A"<<endl;
+	}
+	return 0;
+	
+}
