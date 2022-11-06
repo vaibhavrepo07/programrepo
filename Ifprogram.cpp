b2cdec2aec1c299545f6592e215ebf3b24df1b66
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-06 11:35:54 +0530

    ifProgram

---
+++ Ifprogram.cpp
@@ -0,0 +1,17 @@
+//Program for If Statement 
+#include<bits/stdc++.h>  
+using namespace std;
+int main()
+{
+	int  a; 
+	cin>> a ;
+	cout<<"Coding with If Statement"<<endl;
+	
+	if(a>30)
+	{
+		cout<<"The condition is True"<<endl;
+	}
+	
+	return 0;
+	
+}
