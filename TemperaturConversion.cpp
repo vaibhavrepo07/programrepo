2edc933e08c86e83e1ff0487fa58464bb24974d8
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-06 11:35:04 +0530

    TemperatureConversion

---
+++ TemperaturConversion.cpp
@@ -0,0 +1,19 @@
+//Program for Temperature Conversion 
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int f ,c , a;
+	cin >> f>> c;
+	
+	cout<<"Program for Temperature Conversion "<<endl;
+	
+	c= 9/5*c+32;
+	cout<<"The Clesius to Fahrenheit"<<c<<endl;
+	
+	f=9/4+f-32;
+	cout<<"The Fahrenheit to Celcius"<<f<<endl;
+	
+	return 0;
+}
+
