0256fcb03888673d7cc50e872512defe2c79b8e7
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-06 11:30:27 +0530

    iF3Program

---
+++ If3Program.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int x;
+	
+	cin>>x;
+	cout<<"Enter a Number"<<endl;
+	
+	if(x>100)
+	{
+		cout<<"The value of x is Greater than 100"<<endl;
+	}
+	
+	return 0;
+	
+}
