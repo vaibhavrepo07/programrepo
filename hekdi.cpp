dbe8a49b39dd2615683921eb202ec521cd8d07a7
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-07 13:15:33 +0530

    Patternc1

---
+++ hekdi.cpp
@@ -0,0 +1,16 @@
+//Program for Pattern 10 
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	for(int i=1;i<=7;i++)
+	{
+		for(int j=1;j<=5;j++)
+		{
+			cout<<i<<" "<<j<<" ";
+			
+		} cout<<endl;
+	}
+	return 0 ;
+	
+}
