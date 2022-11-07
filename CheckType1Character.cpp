d63a91b0c997fb4a54701ba1998c1f94cce2e0f7
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-07 13:09:19 +0530

    Checktype1Character

---
+++ CheckType1Character.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	char Z;
+	cin>> Z;
+	cout<<"Coding with Z Character"<<endl;
+	
+	if(Z<=30 && Z<59)
+	{
+		cout<<"The Value of Z is not Good"<<endl;
+	}
+	else if(Z<=90 && Z>=60)
+	{
+		cout<<"The Value of Z is Correct "<<endl;
+	}
+	else if(Z=23 && Z==56)
+	{
+		cout<<"The Value of Z is Abort"<<endl;
+	}
+	
+	return 0 ;
+}
