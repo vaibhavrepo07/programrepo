36c4bb71b207a23625e58f1ab825b2f2fc081d5b
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-12 14:13:19 +0530

    Tableof5

---
+++ Tableof5.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int i=5, j ;
+	cout<<"Table of 5 "<<endl;
+	
+    j=1;
+    while(j<=10)
+    {
+    	cout<<" "<<i*j;
+    	j=j+1;
+	}
+	return 0;
+	
+}
