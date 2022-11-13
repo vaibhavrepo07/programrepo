4d8a30d8d937cc2450bb9260ffcd2c007fccc8ab
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-13 12:26:23 +0530

    BinaryDigits5

---
+++ BinaryDigits5.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n=6;
+	for(int i=1;i<=n;i++)
+	{
+		for(int j=1;j<=n;j++)
+		{
+			cout<<(i%2)<<" ";
+		}
+		cout<<endl;
+	}
+	return 0;
+}
