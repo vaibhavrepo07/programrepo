17052134fe2d1744707bce3cd2881992f81bcdcc
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-14 19:42:19 +0530

    FunctionwithArgumentandNoREturnValue

---
+++ FunctionwithArgumentandNoReturn.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+void add(int , int );
+int main()
+{
+	int a,b;
+	cin>>a>>b;
+	cout<<"Enter the Number"<<endl;
+	add(a,b);
+	
+	return 0;
+}
+   void add(int x ,int y)
+   {
+   	int c;
+   	c = x + y;
+   	cout<<"The addition of Two Number is "<<c;
+   	
+   }
+   
