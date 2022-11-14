221e26f56c58d261eebe2618f348cdb18b407505
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-14 19:46:40 +0530

    RefrenceVAriable

---
+++ REfrencevariable.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int a=100, b;
+	int &ref = a;
+	cout<<"Value of A "<<a <<endl;
+	cout<<"Value of B "<<b <<endl;
+	cout<<"Address of A "<<&a<<endl;
+	cout<<"Address of B "<<&b<<endl;
+	
+	return 0;
+}
