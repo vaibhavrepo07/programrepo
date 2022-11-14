fd94d9e696c8f496a37007039fafcfd0e2602e9d
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-14 19:44:52 +0530

    FunctionWithNoArgumentandNoReturnVAlue

---
+++ FunctionWithNoArgumentandNoreturnvalue.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+void sqr(void);
+int main()
+{
+	int no;
+	cout<<"Enter  a Number ";
+	cin>>no;
+	
+	cout<<"Square of "<<(no*no)<<endl;
+	
+	return 0;
+}
+
