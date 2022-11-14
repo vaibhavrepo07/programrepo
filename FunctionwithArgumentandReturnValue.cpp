06aeda284170d2cc54fe1ffc077b4587114cdaf6
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-14 19:43:11 +0530

    FunctionwithArgumentandReturnvalue

---
+++ FunctionwithArgumentandReturnValue.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int sqrt(int);
+int main()
+{
+	int no, ans ;
+	cout<<"Enter a Number";
+	cin>>no;
+	ans=sqrt(no);
+	cout<<"Square of "<<no<<"is"<<ans;
+	
+	return 0;
+}
+    int sqrt(int x)
+    {
+         return (x*x);
+	}
+
+
