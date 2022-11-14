cc26e3f25710ba5b48fbda24c043c08b9b4834eb
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-14 19:39:00 +0530

    FunctionDeclarationadnFunctiondefination

---
+++ FunctionDeclaratonandFunctionDefination.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+void disp();  //function prototype
+int main()
+{
+	disp();
+	
+	return 0;
+}
+void disp()   //function defination
+{
+	cout<<"Hello"<<endl;
+}
