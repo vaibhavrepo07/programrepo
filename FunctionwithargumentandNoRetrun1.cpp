eb7df65156622527af67e7424e12871b96fcf965
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-16 20:47:23 +0530

    FunctionwithargumentandNoReturnValue1

---
+++ FunctionwithargumentandNoRetrun1.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+void Int_sum(int );
+int main()
+{
+	int x;
+     Int_sum(x);
+     
+     return 0;
+}
+   
+    void Int_sum(int x)
+    {
+    	int a,b,z;
+    	cout<<"Enter the two Numbers for Summation"<<endl;
+    	cin>>a>>b;
+    	z=a+b;
+    	cout<<"The Total of Two Numbers is ="<<z<<endl;
+    	
+	}
