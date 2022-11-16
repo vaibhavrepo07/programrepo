79a48446de8d0065cbe7c5649ae82dd326259aa6
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-16 20:44:30 +0530

    CallByValueusingFunction1

---
+++ CallbyValueuisngFunction1.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void cube(int );
+int main()
+{
+	int a =10 ;
+	
+	cube(a);
+	cout<<" a is "<<a<<endl;
+	
+	return  0;
+}
+  void cube(int x)
+  {
+  	x = x*x*x;
+  	cout<<"x is "<<x<<endl;
+  	
+  }
