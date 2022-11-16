903a10fa94768c5e57c2d8773ff7bfba6c62a7b7
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-16 20:57:02 +0530

    DigitsInBinary1

---
+++ DigitsinBinary1.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n=5;
+	
+	for(int i=2;i<=n;i++)
+	{
+		for(int j=3;j<=n;j++)
+		{
+			cout<<(j%3)<<endl;
+		}
+		cout<<endl;
+		
+	}
+	return 0;
+}
