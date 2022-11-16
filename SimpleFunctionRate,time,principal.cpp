df041ba8f046e205762914287ebe10dbf5f2e7d7
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-16 20:49:12 +0530

    SimpleFunctionRate,Time,principal

---
+++ SimpleFunctionRate,time,principal.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+void Int_swap();
+int main()
+{
+	 Int_swap();
+	 
+	 return 0;
+}
+void Int_swap()
+{
+	int t, r, p, si;
+	cout<<"Enter Principal , Rate , Time"<<endl;
+	cin>> p >> r>>t;
+	
+	si=(t*p*r);
+	
+	cout<<si<<endl;
+	
+}
