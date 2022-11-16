6bf06a6bb7b4508f98b7579e69c58599887afde1
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-16 20:48:21 +0530

    FunctionWithReturnValueandArgument1

---
+++ FunctionwithReturnValueandArgument1.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int book_Int(int ,int );
+int main()
+{
+	 int a,b;
+	 
+	 book_Int(a,b);
+	 
+	 return 0;
+}
+ book_Int(int x,int y)
+ {
+ 	int a,b,r;
+ 	cout<<"Coding with two Number"<<endl;
+ 	cin>>a>>b;
+ 	r=a+b;
+ 	return r;
+ }
