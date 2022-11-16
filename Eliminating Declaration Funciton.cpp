f25fc51b74eb0e21dd6b76e92f7ae82c3cbee7c8
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-16 20:45:26 +0530

    Eliminating Declaration function

---
+++ Eliminating Declaration Funciton.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//Eliminating Declaration 
+
+  void mul(int x)
+  {
+  	cout<<" X is used for Multiplication"<<x<<endl;
+  	
+  }
+  
+   int main()
+   {
+   	  int z=10;
+   	  cout<<"The square is ="<<z*z;
+   	  mul(z);
+   	  
+   	  return 0;
+   }
