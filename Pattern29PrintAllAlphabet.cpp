ee921050912adfb42aa52fda175e2ebe04e74390
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-17 21:15:57 +0530

    Pattern29PrintAllAlphabet

---
+++ Pattern29PrintAllAlphabet.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	char i;
+	for(int i='A';i<='Z';i++)
+	{
+		for(int j='A';j<='Z';j++)
+		{
+			
+		cout<<char (j)<<" ";
+	}
+	cout<<endl;
+}
+	return 0;
+}
