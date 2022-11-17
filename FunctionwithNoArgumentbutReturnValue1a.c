799a1499f9902de487a1d154bb43c8d1df180f9c
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-17 21:27:46 +0530

    Pattern35

---
+++ FunctionwithNoArgumentbutReturnValue1a.c
@@ -0,0 +1,19 @@
+ #include<bits/stdc++.h>
+ using namespace std;
+ float calc_Fun(void);
+ void main()
+ {
+ 	float c;
+ 	calc_Fun();
+ 	
+ 	cout<<c;
+ 	
+ }
+ float calc_Fun(void)
+ {
+ 	float a,b,c;
+ 	cin>>a>>b;
+ 	c = a * b;
+ 	
+ 	return c;
+ }
