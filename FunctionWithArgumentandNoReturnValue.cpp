533a79b2051894d3ba1042895855577ba8285d78
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-17 21:10:43 +0530

    FunctionwithArgumentandNoREturnValue

---
+++ FunctionWithArgumentandNoReturnValue.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+void Fun_Int(int , int );
+int main()
+{
+	int a, b,c;
+	Fun_Int(a,b);
+	cout<<c<<endl;
+	
+	return 0;
+}
+
+   void Fun_Int(int x ,int y)
+   {
+   	 int c;
+   	 cin>>x>>y;
+		c = x+y;
+   	 cout<<"The Value of x and b is "<<c<<endl;
+   }
