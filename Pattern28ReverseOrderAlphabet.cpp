acb54ffcee3f7eed6040e39e2a0c156bbb528218
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-17 21:13:56 +0530

    Pattern28ReverseOrderAlphabet

---
+++ Pattern28ReverseOrderAlphabet.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	char i,j;
+	for(int i='Z';i>='T';i--)
+	{
+		for(int j='Z';j>='T';j--)
+		{
+			cout<<char(j)<<" ";
+		}
+		cout<<endl;
+	}
+	return 0;
+}
