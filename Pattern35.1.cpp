e29c8062356d962229101f9b0ed4f4e938f62d38
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-18 20:37:00 +0530

    Pattern34.1

---
+++ Pattern35.1.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n=5;
+	for(int i=1;i>=1;i--)
+   {
+   	for(int j=1;j>=1;j--)
+   	{
+   		cout<<i<<" ";
+	   }
+	   cout<<endl;
+   }
+   return 0;
+}
