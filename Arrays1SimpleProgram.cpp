f91c545d91d5126bf96c03918007cb4f590a272c
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-18 20:34:32 +0530

    Arrays1SimpleProgram

---
+++ Arrays1SimpleProgram.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#define N 15
+using namespace std;
+int main()
+{
+	int i,a[N];
+	for(i=0;i<N;i++)
+	{
+		cout<<i<<endl;
+		cin>>a[i];
+	}
+	for(i=0;i<N;i++)
+	{
+		cout<<a[i];
+		cout<<endl;
+	}
+	return 0;
+}
