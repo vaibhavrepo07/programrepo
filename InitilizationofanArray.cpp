eee48f541955be83d2adac0e038888b98ee4848d
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-19 21:40:49 +0530

    InitilizationOfanArray

---
+++ InitilizationofanArray.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int i, a[5]={10,20,30,40,50};
+	cout<<"Enter the INitilization of An Array"<<endl;
+	for(i=0 ;i<5 ;i++)
+	{
+		cout<<"  "<<(i+1)<< ")" <<"-->"<< a[i] <<endl;
+	}
+	
+	return 0;
+}
