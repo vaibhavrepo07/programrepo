cc42ce5bbf776b63156e53a2636d15702e65ac9e
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-19 21:43:11 +0530

    SimpleArray

---
+++ SimpleArray,cpp.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int a[5];
+	int i, sum=0;
+	cout<<"Simple Array Program "<<endl;
+	for(i=0;i<5;i++)
+	{
+		cin>>a[i];
+	}
+	for(i=0;i<5;i++)
+	{
+		cout<<a[i];
+	}
+	return 0;
+}
