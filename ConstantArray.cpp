eca742bd3ebef7233412c67a5f66f86f597b362e
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-19 21:39:53 +0530

    ConstantArray

---
+++ ConstantArray.cpp
@@ -0,0 +1,11 @@
+//Program for Constant Array
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	const int a[5]={4,3,5,4,3};
+	cout<<"Printing the Constant Array"<<endl;
+	cout<<a[3];
+	
+	return 0;
+}
