f7d7771fd5307ac0c9f7a80d296aadddd46e3123
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-20 20:40:03 +0530

    Pattern41aa

---
+++ Pattern41aa.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n=5;
+	for(int i=1;i<=4;i++)
+	{
+		for(int j=2;j<=i;j++)
+		{
+			cout<<j<<" ";
+		}
+		cout<<endl;
+	}
+	return 0;
+}
