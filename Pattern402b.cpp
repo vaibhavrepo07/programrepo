052c116a14b17b52c9e7e2ef02d792788bfe8de1
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-20 20:41:51 +0530

    Pattern402b

---
+++ Pattern402b.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n=5;
+   
+    for(int i=2;i<=n;i++)
+    {
+    	for(int j=2;j<=n;j++)
+    	{
+    		cout<<(j)<<endl;
+		}
+		cout<<endl;
+	}
+	return 0;
+}
