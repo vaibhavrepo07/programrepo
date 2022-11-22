4e25f4e497903def9d50143822e9c7a5d2096b2a
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-22 22:59:58 +0530

    PointerandString

---
+++ PointerandString.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char s[15]= "Hello World";
+	char *ptr = s;
+	cout<<ptr;
+	ptr++;
+	cout<<ptr;
+	
+	ptr=ptr+1;
+	cout<<ptr;
+	
+	return 0;
+}
