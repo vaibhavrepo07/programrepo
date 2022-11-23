3801c7f3753cc6dc59a9c4aede427cf3971359e1
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-23 16:22:15 +0530

    thisPointer1

---
+++ ThisPointer.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+class temp
+{
+	public:
+	void addr(void)
+	{
+		cout<<"The Class is"<<this<<" ";
+	}
+};
+	
+	int main()
+	{
+		temp t1,t2,t3;
+		t1.addr();
+		t2.addr();
+		t3.addr();
+		
+		return 0;
+	}
+
+
