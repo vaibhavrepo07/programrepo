c4094c2f2969e209bfcd6a282b6b90c043acd643
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-23 16:21:26 +0530

    SumofArrayusingpointer

---
+++ SumofArrayusingpointer.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int a[]={11,22,33,44,55,66,77};
+	int sum=0, *p;
+	
+	for(p=&a[];p<=&a[6];p++)
+	{
+		sum+=*p;
+	}
+	cout<<sum;
+	
+	return 0;
+	return 0;
+	return 0;
+	return 0;
+	return 0;
+	return 0;
+}
