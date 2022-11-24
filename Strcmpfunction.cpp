7a0510f4e6de47c6085158bc1e5f6851f379c104
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-24 21:48:33 +0530

    StrcmpFunction

---
+++ Strcmpfunction.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	char *s1="abce";
+	char *s2="bbce";
+	
+	if(strcmp(s1,s2)<0)
+	cout<<"s1 is less than s2"<<endl;
+	else
+	cout<<"s1 is greater than or equal to s2"<<endl;
+	
+	return 0;
+}
