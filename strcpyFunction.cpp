18ff904e5b76ff9d5211ec355e5770f8cc196218
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-24 21:51:04 +0530

    StrcpyFunction

---
+++ strcpyFunction.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	char str1[10]="Hello";
+	char str2[10];
+	
+	cout<<strcpy(str2,str1)<<" "<<endl;
+	cout<<str2<<" "<<endl;
+	
+	return 0;
+}
