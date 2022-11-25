0ca71e2ab85ac980360c133fe62abd77ef92e679
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-25 23:06:11 +0530

    strRev(stringCompar)

---
+++ StrRev(stringCompare).cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	char s[30]="Hello World Vaibhav";
+	
+	cout<<"Original String"<< s<<endl;
+	strrev(s);
+	cout<<"String after strrev"<<s<<endl;
+	
+	return 0;
+}
