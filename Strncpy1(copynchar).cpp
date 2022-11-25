a4b273ee5a185fdefe96d1e82c8800f78cfc90fa
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-25 23:04:44 +0530

    strncpy1(copynchar)

---
+++ Strncpy1(copynchar).cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	char src_str[30]= "Hello Students";
+	char dest_str[30];
+	
+	cout<<"src_str"<<src_str<<endl;
+	strncpy(dest_str, src_str,5);
+	cout<<"dest_str:"<<dest_str<<endl;
+	
+	strncpy(dest_str, "Copy first 13 characters",13);
+	cout<<"dest_str"<<dest_str<<endl;
+	
+	return 0;
+}
