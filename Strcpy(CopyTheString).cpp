9de2d65bceb68cce0531ef02c7a3316710f0e441
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-25 22:58:21 +0530

    Strcpy(CopyTheString)

---
+++ Strcpy(CopyTheString).cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	char str_input[10]={"Vaibhav"};
+	char str_output[10];
+	
+	cout<<"str_input"<<str_input<<" "<<endl;
+	strcpy(str_output,str_input);
+	
+	return 0;
+}
