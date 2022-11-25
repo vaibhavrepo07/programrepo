2f2b77b509221ddcd8a795b8101182d37d567027
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-25 23:01:32 +0530

    Strlen(StringLength)1

---
+++ Strlen(stringLength)1.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	char s[30];
+	int i;
+	cout<<"Enter the String"<<endl;
+	cin.getline(s,30);
+	i=strlen(s);
+	cout<<"The Length of the string is "<<i <<endl;
+	
+	return 0;
+}
