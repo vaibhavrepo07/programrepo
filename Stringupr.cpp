3f76375c75ef4cff6006790afc66ad9bdbce5fdc
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-26 23:24:33 +0530

    Stringupr

---
+++ Stringupr.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	char str[20]="happy birthday";
+	
+	cout<<"original String is"<<str<<endl;
+     strupr(str);
+     cout<<" String after strupr"<<str<<endl;
+     
+     return 0;
+}
