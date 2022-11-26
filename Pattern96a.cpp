5588256333e79db991ba8f758b64968e78843702
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-26 23:19:11 +0530

    Pattern96a

---
+++ Pattern96a.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n=5;
+	int i ,j , k ;
+	
+	for(int i=1;i<=n;i++)
+{
+	for(int j =n-1;j>=i;j++)
+	{
+		cout<<" ";
+	}
+	for(int k=1;k<=i;k++)
+	{
+		cout<<k;
+	}
+	cout<<endl;
+}
+return 0;
+}
