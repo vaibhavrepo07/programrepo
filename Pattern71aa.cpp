99ece61e6bfebf7db0b15530ec4e39724e9e419d
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-27 19:30:22 +0530

    Pattern71aa

---
+++ Pattern71aa.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	for(int i=1;i<=5;i++)
+	{
+		for(int j=5;j>=1;j--)
+		{
+			cout<<i+1<<" "<<endl;
+		}
+		cout<<endl;
+	}
+	return 0;
+}
