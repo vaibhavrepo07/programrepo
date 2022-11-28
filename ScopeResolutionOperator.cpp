87c8f07d6c4ea1250065073afe4d290edcfd578d
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-28 23:12:15 +0530

    ScopeResolutionOperator

---
+++ ScopeResolutionOperator.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int sum=100;
+int main()
+{
+	int sum=30;
+	
+	cout<<"The VAlue of Global sum is ="<<::sum<<endl;
+	cout<<"The value of Local Sum is = "<<sum   <<endl;
+	
+	return 0;
+}
