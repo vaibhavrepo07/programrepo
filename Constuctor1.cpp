7ace68745f38a7667e20cd3ad8424b0aeec369c8
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-28 23:08:13 +0530

    Constructor1a

---
+++ Constuctor1.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+class first
+{
+	int a,b;
+	public:
+		 first () //calling constuctor 
+		
+		{
+			a=0;
+			b=0;
+		}
+		void disp(void)
+		{
+			cout<<"Value of A "<<a <<endl;
+			cout<<"Value of B "<<b  <<endl;
+		}
+	};
+		
+		int main()
+		{
+			first t;
+			t.disp();
+			
+			return 0;
+		}
+		
+
