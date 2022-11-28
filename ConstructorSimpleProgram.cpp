25afb8eae43c0767e30850e32b5d11dd765720a9
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-11-28 23:07:17 +0530

    ConstructorsimpleProgram

---
+++ ConstructorSimpleProgram.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+class feel
+{
+	int a,b;
+	public:
+		 feel()
+		{
+			cout<<"Feeling Jovial"<<" "<<endl;
+			a=0;
+			b=0;
+		}
+		void disp(void)
+		{
+			cout<<"The Value of A is ="<<a<<endl;
+			cout<<"The value of B is ="<<b<<endl;
+		}
+		
+	};
+		int main(
+		{
+			feel t1;
+			t1.disp();
+			
+			return 0;
+		 } 
+
