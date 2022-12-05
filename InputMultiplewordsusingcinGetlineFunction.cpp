a2c3b5f04ade6f96e0ce4a883060d43562cb7fe4
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-12-05 22:12:20 +0530

    InputgetlineFunction1

---
+++ InputMultiplewordsusingcinGetlineFunction.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	 
+	  char str[30];
+	  cout<<"Coding with string Cin.getline";
+	  
+	  cin.getline(str,10);
+	  
+	  cout<<"The VAlue is ="<<str;
+	  
+	  return 0;
+	  
+}
