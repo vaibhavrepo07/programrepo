c4f4d61169550fdcb841ad977b722e4a2f79b13a
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-12-05 22:11:24 +0530

    InputMultipleWordsUsinginFunction1

---
+++ inputMUltipleWordsUsingcinfunction1.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int max=200;
+
+int main()
+{
+	int max;
+	char str[max];
+	
+	cout<<"Enter the Values"<<endl;
+	
+	cin.get(str,max,'$');
+	
+	cout<<"The Max value is "<<max;
+	cout<<"The Str value is "<<str<<endl;
+	cout<<"The Dollor is "<<'$'<<endl;
+	
+	return 0;
+}
