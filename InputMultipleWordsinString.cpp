73790ce8ac80ded2c6dee6dc48e9ebaa7042b9d4
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-12-05 22:03:10 +0530

    InputMultipleWordsinString

---
+++ InputMultipleWordsinString.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int max=40;
+
+int main()
+{
+	int max;
+	char str[max];
+	
+	cin.get(str,max);
+	
+	cout<<"The Value of Max is"<<max<<endl;
+	cout<<"The Value of Str is"<<str<<endl;
+	
+	return 0;
+}
