4ed5417b0865e840894793c2cc3c0e223f770284
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-12-05 22:10:00 +0530

    InputMultiplyWordsStringsusingcin.get

---
+++ InputMultipleWordsstringusingcin.get().cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int max=200;
+
+int main()
+{
+	 int max;
+	 char str[max];
+	 
+	 cout<<"Enter the value of String INput multiple Word using cin.get";
+	 cout<<"The Value is ('$\')";
+	 
+	 cin.get(str,max,'$');
+	 
+	 cout<<"Entered String is"<<str;
+	 cout<<"Entered String 1 is "<<max;
+	 cout<<"Entered String 2 is"<<'$';
+	 
+	 return 0;
+}
