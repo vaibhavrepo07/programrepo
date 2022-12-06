a2c6881f2ce03479a6be1c178f94abd86c6f8457
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-12-06 23:33:43 +0530

    Pattern192

---
+++ Pattern192.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n=5;
+	
+	for(int i=1;i<=n;i++)
+	{
+		for(int j=1;j<=n;j++)
+		{
+			if(j<=n-i+1)
+			cout<<(i+j-1)<<" ";
+			else
+			cout<<(n-j+1)<<" ";
+		}
+		cout<<endl;
+	}
+	return 0;
+}
