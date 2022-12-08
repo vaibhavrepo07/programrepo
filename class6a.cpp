c5bb87f8d311f19c66c523d5fae31157f3b55ec7
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-12-08 22:46:09 +0530

    class6a

---
+++ class6a.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+class marvellous
+{
+	public:
+		void display()
+		{
+			cout<<"Your Speech was Marvellous";
+		}
+		
+};
+   
+     int main()
+     {
+     	 marvellous versatile;
+     	 versatile.display();
+     	 
+     	 return 0;
+	 }
