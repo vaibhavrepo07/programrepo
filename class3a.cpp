865e9ae049cbd9de27732e8904bf68dc0ace92b2
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-12-08 22:43:57 +0530

    class3a

---
+++ class3a.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+class car 
+{
+	public:
+		 void display()
+		 {
+		 	cout<<"Buggati Chiron is Exorbitant"<<endl;
+		 	cout<<"For Purchase this you need, No Stone Unturned "<<" "<<endl;
+	}
+};
+  
+    
+    int main()
+    {
+    	 car chiron;
+    	 chiron.display();
+    	 
+    	 return 0;
+	}
