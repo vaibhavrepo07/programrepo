42272a815e3486c3f2603bf607efd192858ee49b
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-12-08 22:42:38 +0530

    Class1a

---
+++ class1a.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+class bike 
+{
+	public:
+		void display()
+		{
+			cout<<"India's Most Satisfying Bike is = "<<endl;
+			cout<<"That's Hayabusa ."<<endl;
+		}
+};
+
+ int main()
+ {
+ 	 bike suzuki;
+ 	 suzuki.display();
+ 	 
+ 	 return 0;
+ 	 
+ }
