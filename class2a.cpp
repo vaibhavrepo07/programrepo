a1e7ad043838701cfd1b4968a8240232f861e063
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-12-08 22:43:15 +0530

    class2a

---
+++ class2a.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+class laptop
+{
+	public:
+		void display(void)
+		{
+			cout<<"The Laptop is abbriviation ="<<endl;
+			cout<<"It's stands for ('Light Weight Analitical Platform for Total Optimized Power.')"<<" "<<endl;
+		}
+	};
+	
+	 int main()
+	 {
+	 	laptop dell;
+	 	dell.display();
+	 	
+	 	return 0;
+	 }
+
