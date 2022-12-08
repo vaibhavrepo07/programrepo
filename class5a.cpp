450b22a5c88e3a8aabc0295a2e93e4e26c454858
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-12-08 22:45:28 +0530

    class5a

---
+++ class5a.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+class universe
+{
+	public:
+		void display()
+		{
+			cout<<"MilkyWay is our Galaxy"<<endl;
+			cout<<"Andromeda is our Neighbour Galaxy ";
+		}
+};
+
+   int main()
+   {
+   	 
+   	   universe star ;
+   	   star .display();
+   	   
+   	   return 0;
+   }
