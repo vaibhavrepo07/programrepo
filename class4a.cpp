033352b43fe84c4b7a6c69d91ac866f5ba073524
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-12-08 22:44:52 +0530

    class4a

---
+++ class4a.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+class first 
+{
+	public:
+		void display()
+		{
+			cout<<"Hello, C++ Programmar  "<<endl;
+		    cout<<"It's my Adorable Programming Language for Competitive Programmar";
+		}
+};
+
+ 
+  int main()
+  {
+  
+  first day ;      //object created 
+  day.display();
+  
+  return 0 ;
+}
+
