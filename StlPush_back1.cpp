410d53718d6a62d2809d6f4fa59e0b0a07af39d4
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-12-13 21:16:02 +0530

    StlPush_back1

---
+++ StlPush_back1.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	 vector <int>a;
+	 
+	 a.push_back(4);
+	 vector<int>b(a);
+	 b[0]=5;
+	 
+	// vector<char>c(a);
+	
+	cout<<b[0];
+	 
+	 return 0;
+}
