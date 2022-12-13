261802d1ff0184e1d2312a8e808fa39a7b042ea7
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-12-13 21:25:02 +0530

    Constructora1

---
+++ Constructora1.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+class integer
+{
+	int a,b;
+	public:
+		integer(void);
+};
+
+   integer::integer(void)
+   {
+      a=10;
+      b=10;
+   }
+   
+   int main()
+   {
+   	
+   	return 0;
+   }
