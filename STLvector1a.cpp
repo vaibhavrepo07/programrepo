49994021e847808f619cbdf065617010ac0d3fb6
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2022-12-13 21:12:38 +0530

    STLvector1a

---
+++ STLvector1a.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	 vector <int> a;
+	 
+	 a.push_back(2);
+	 
+	 vector<int>b(a);
+	 b[0]=6;
+	 cout<<b[0]<<" "<<endl;
+	 
+     vector <char>c(n:4 , x:68);
+	 
+	 	 
+	 return 0;
+}
