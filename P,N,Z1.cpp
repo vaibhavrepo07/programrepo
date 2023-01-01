4fb1c33a65ad008c1311f32313e3133645ee003f
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-01-01 19:37:22 +0530

    P,N,Z1

---
+++ P,N,Z1.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int a;
+	cout<<"Enter the code for to check the value is Zero or Negative or Positive"<<endl;
+	cin>>a;
+	if(a==0)
+	cout<<"The given number is Zero ";
+	else if(a>0)
+	cout<<"The number is Positive "<<endl;
+	else
+	cout<<"The number is Negative "<<endl;
+	
+	return 0;
+}
