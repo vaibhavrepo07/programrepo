d27ef6057dc63d853a12f06bfcfaac1971611907
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-01-01 19:36:39 +0530

    MinandMaxvalue

---
+++ MinandMaxvalue.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int a,b;
+	cout<<"Coding with Min and Max Number "<<endl;
+	cin>>a>>b;
+	
+	if(a>b)
+	cout<<" The value of  a is Greater than b"<<endl;
+	else
+	cout<<" The value of b is Greater than a "<<endl;
+	
+	return 0;
+}
