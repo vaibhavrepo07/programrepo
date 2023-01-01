e5eba5cec805ccc5b7e7972c9e99f47541913d71
vaibhavrepo07 <vaiprogrammer.v762000@gmail.com>
2023-01-01 19:39:55 +0530

    tableof3usingwhile

---
+++ tableof3usingwhileloop.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int i=5;
+    cout<<"The table of 5 "<<endl;
+    while(i<=50)
+    {
+    	cout<<" "<<i<<endl;
+    	i+=5;
+	}
+	
+	return 0;
+}
